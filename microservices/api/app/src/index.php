64793d6c493fd8630ce94580dbcaf5b1e3ea886e
dvkcool <divyanshukumarg@gmail.com>
2018-01-19 21:37:46 +0530

    tr28

--- microservices/api/app/src/index.php
+++ microservices/api/app/src/index.php
@@ -1,6 +1,6 @@
 <?php
  
-require_once __DIR__ . '/vendor/autoload.php';
+require_once  '/vendor/autoload.php';
  
 // use the factory to create a Faker\Generator instance
 $faker = Faker\Factory::create();
