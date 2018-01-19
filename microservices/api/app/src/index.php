323088e119de678f108bcfcba28a34f701a34868
dvkcool <divyanshukumarg@gmail.com>
2018-01-19 23:55:31 +0530

    lets hope close

--- microservices/api/app/src/index.php
+++ microservices/api/app/src/index.php
@@ -4,12 +4,15 @@ require_once  __DIR__ .'/vendor/autoload.php';
  
 // use the factory to create a Faker\Generator instance
 $faker = Faker\Factory::create();
-  
+
+echo "Fake name:";  
 // generate data by accessing properties
 echo $faker->name;
-  
+
+echo "\n Fake address:";  
 // generate fake address
 echo $faker->address;
-  
+ 
+echo "\n Fake text:";
 // generate fake name
 echo $faker->text;
